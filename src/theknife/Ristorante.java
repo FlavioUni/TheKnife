afd9c9227c802592a2b0d3cf94aa440aef14e153
Scolaro Gabriele <gscolaro1@studenti.uninsubria.it>
2025-05-08 13:21:28 +0200

    aggiornamento classe Ristorante

--- src/theknife/Ristorante.java
+++ src/theknife/Ristorante.java
@@ -2,4 +2,16 @@ package theknife;
 
 public class Ristorante {
 
+	private String nome;
+	private String indirizzo;
+	private String posizione;
+	private double prezzo;
+	private enum cucina;
+	private double longitudine;
+	private double latitudine;
+	private String numeroTelefono;
+	private String url;
+	private String urlSito;
+	private 
+	
 }
