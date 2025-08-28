04c650afd10cd0857603e36bdeb157ab856931fd
Gasparini Lorenzo <lgasparini@studenti.uninsubria.it>
2025-08-28 15:16:01 +0200

    Aggiustamento gestione eccezioni

---
+++ src/eccezioni/InputAnnullatoException.java
@@ -0,0 +1,8 @@
+package eccezioni;
+
+public class InputAnnullatoException extends RuntimeException {
+	public InputAnnullatoException() {
+        super("Input annullato dall'utente.");
+	}
+}
+// Crea un'istanza dell'eccezione con il messaggio "Input annullato dall'utente".
