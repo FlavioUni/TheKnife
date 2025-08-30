b360d2b3c32e4c8573027ff28537a84022ef55b9
Scolaro Gabriele <gscolaro1@studenti.uninsubria.it>
2025-08-30 13:17:47 +0200

    classe RecensioneService

---
+++ src/theknife/logica/RecensioneService.java
@@ -0,0 +1,17 @@
+/*
+Ciani Flavio Angelo, 761581, VA
+Scolaro Gabriele, 760123, VA
+Gasparini Lorenzo, 759929, VA
+*/
+
+package theknife.logica;
+
+import java.util.List;
+
+import theknife.recensione.Recensione;
+
+public class RecensioneService {
+	
+	private List<Recensione> recensioni;
+
+}
